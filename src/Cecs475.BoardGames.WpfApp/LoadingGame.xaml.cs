02655b253a5173e5826cd1c0067d5e599cf707c9
Ajay Kc <azay.kc13@gmail.com>
2018-05-10 11:48:52 -0500

    JSON to class

--- src/Cecs475.BoardGames.WpfApp/LoadingGame.xaml.cs
+++ src/Cecs475.BoardGames.WpfApp/LoadingGame.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Cecs475.BoardGames.WpfView;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -20,6 +23,26 @@ namespace Cecs475.BoardGames.WpfApp
     /// <summary>
     /// Interaction logic for LoadingGame.xaml
     /// </summary>
+
+    public class GameType
+    {
+        public GameFiles[] gameFiles { get; set; }
+    }
+
+    public class GameFiles
+    {
+        public string Name { get; set; }
+        public File[] Files { get; set; }
+    }
+
+    public class File
+    {
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public string PublicKey { get; set; }
+        public string Version { get; set; }
+    }
+
     public partial class LoadingGame : Window
     {
         public LoadingGame()
@@ -29,11 +52,34 @@ namespace Cecs475.BoardGames.WpfApp
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var client = new RestClient("https:///cecs475-boardgames.herokuapp.com");
+            var client = new RestClient("https://cecs475-boardamges.herokuapp.com");
             var request = new RestRequest("api/games", Method.GET);
 
-            var task = client.ExecuteTaskAsync(request);
-            var response = await task;
+            var response = await client.ExecuteTaskAsync<GameType>(request);
+
+            var gameList = response.Data;
+            //var response = await client.ExecuteTaskAsync(request);
+
+            //JObject obj = JObject.Parse(response.Content);
+
+            //var gameList = JsonConvert.DeserializeObject<List<IWpfGameFactory>>(response.Content);
+
+            foreach (var game in gameList.gameFiles)
+            {
+               
+                Debug.WriteLine(game.Name);
+                string remoteUri = "";
+                string fileName = "";
+                string webResource = remoteUri + fileName;
+
+                WebClient mWebClient = new WebClient();
+                var downloadTask = mWebClient.DownloadFileTaskAsync(webResource, fileName);
+
+                await downloadTask;
+            }
+
+            //var task = client.ExecuteTaskAsync(request);
+            //var response = await task;
 
             /*if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
@@ -45,16 +91,9 @@ namespace Cecs475.BoardGames.WpfApp
             }*/
 
             //needs to be inside a foor loop
-            string remoteUri = "";
-            string fileName = "";
-            string webResource = remoteUri + fileName;
-
-            WebClient mWebClient = new WebClient();
-            var downloadTask = mWebClient.DownloadFileTaskAsync(webResource, fileName);
-
-            await downloadTask;
             
-            
+
+
 
             Window.GetWindow(new GameChoiceWindow()).Show();
             this.Close();
